37a50fccf99f19144478e45efd194785bca0d79e
Sarah Ramadan Fahmy <sarah.fahmi@moj.gov.sa>
2023-12-25 17:39:59 +0200

    add decorator pattern example

---
+++ DP_Practice/Decorator_2/Implementation.cs
@@ -0,0 +1,75 @@
+﻿namespace Decorator
+{
+    /// <summary>
+    /// Component
+    /// </summary>
+    public interface IMailService
+    {
+        bool SendMail(string message);
+    }
+    /// <summary>
+    /// Concrete Component
+    /// </summary>
+    public class CloudMailService : IMailService
+    {
+        public bool SendMail(string message)
+        {
+            Console.WriteLine("CloudMailService {0}", message);
+            return true;
+        }
+    }
+    /// <summary>
+    /// Concrete Component
+    /// </summary>
+    public class OnPremiseMailServicer : IMailService
+    {
+        public bool SendMail(string message)
+        {
+            Console.WriteLine("OnPremiseMailServicer {0}", message);
+            return true;
+        }
+    }
+    /// <summary>
+    /// Decorator
+    /// </summary>
+    public abstract class MailDecoratorBase : IMailService
+    {
+        private readonly IMailService _mailService;
+        public MailDecoratorBase(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+        public virtual bool SendMail(string message)
+        {
+            return _mailService.SendMail(message);
+        }
+    }
+    public class StatisticMailService : MailDecoratorBase
+    {
+        public StatisticMailService(IMailService mailService) : base(mailService)
+        {
+        }
+        public override bool SendMail(string message)
+        {
+            Console.WriteLine("StatisticMailService {0}", message);
+            return base.SendMail(message);
+        }
+    }
+    public class DatabaseMailService : MailDecoratorBase
+    {
+        public List<string> databases = new List<string>();
+        public DatabaseMailService(IMailService mailService) : base(mailService)
+        {
+        }
+        public override bool SendMail(string message)
+        {
+            Console.WriteLine("DatabaseMailService {0}", message);
+            if (base.SendMail(message))
+            {
+                databases.Add(message);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ DP_Practice/Decorator_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Decorator;
+
+Console.WriteLine("Hello, World!");
+
+var cloudService = new CloudMailService();
+cloudService.SendMail("first");
+
+var onPremise = new OnPremiseMailServicer();
+onPremise.SendMail("first");
+
+
+var statistic = new StatisticMailService(cloudService);
+statistic.SendMail("second");
+
+
+var statistic2 = new StatisticMailService(onPremise);
+statistic2.SendMail("second");
+
+
+var db = new DatabaseMailService(cloudService);
+db.SendMail("third");
+foreach (var item in db.databases)
+{
+    Console.WriteLine("i = {0}", item);
+}
+
+var db2 = new DatabaseMailService(onPremise);
+db2.SendMail("third");
